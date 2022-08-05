a51f54dd044fb323c960873a56d219c9bf32a5fd
dariancmagee <dariancmagee@gmail.com>
2022-08-04 21:50:00 -0400

    finished player jumps

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+// This controls how quickly the player comes down from a jump
+const gravity = 0.7
+
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.lastKey
     }
 
 // This fills in the background of the game
@@ -64,11 +67,13 @@ const keys = {
     d: {
         pressed: false
     },
-    w: {
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
         pressed: false
     }
 }
-let lastKey
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -78,11 +83,20 @@ function animate() {
     enemy.update()
 
     player.velocity.x = 0
+    enemy.velocity.x = 0
 
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -1
-    } else if (keys.d.pressed && lastKey === 'd') {
-        player.velocity.x = 1
+// Player movement
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5
+    }
+
+// Enemy player movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -5
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 5
     }
 }
 
@@ -90,18 +104,30 @@ animate()
 
 // Event listener that moves the players when keys are pressed
 window.addEventListener('keydown', (event) => {
+    console.log(event.key)
     switch (event.key) {
         case 'd':
         keys.d.pressed = true
-        lastKey = 'd'
+        player.lastKey = 'd'
         break
         case 'a':
         keys.a.pressed = true
-        lastKey = 'a'
+        player.lastKey = 'a'
         break
         case 'w':
-        keys.w.pressed = true
-        lastKey = 'w'
+        player.velocity.y = -20
+        break
+
+        case 'ArrowRight':
+        keys.ArrowRight.pressed = true
+        enemy.lastKey = 'ArrowRight'
+        break
+        case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
+        enemy.lastKey = 'ArrowLeft'
+        break
+        case 'ArrowUp':
+        enemy.velocity.y = -20
         break
     }
     console.log(event.key);
@@ -115,8 +141,15 @@ window.addEventListener('keyup', (event) => {
         case 'a':
         keys.a.pressed = false
         break
-        case 'w':
-        keys.w.pressed = false
+    }
+
+// Enemy keys
+    switch (event.key) {
+        case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+        break
+        case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false
         break
     }
     console.log(event.key);
